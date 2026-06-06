bddf7b59d1be38f3980b9625a78d0ad5a08c9812
SimcoIntel <oneinfoservice@gmail.com>
2026-06-06 20:07:29 +0530

    Add -n support to echo

--- coreutils/src/echo.rs
+++ coreutils/src/echo.rs
@@ -1,10 +1,31 @@
 #![no_std]
 #![no_main]
-use libsarga::{sarga_main, print, println};
+extern crate alloc;
+use libsarga::{sarga_main, print, println, args};
 
 fn user_main() {
-    println!("echo output (placeholder)");
-    
-}
+    let mut first = true;
+    let mut newline = true;
+    let mut start = 1usize;
 
+    if args::argc() > 1 {
+        if let Some(flag) = args::get(1) {
+            if flag == "-n" {
+                newline = false;
+                start = 2;
+            }
+        }
+    }
+
+    for i in start..(args::argc() as usize) {
+        if let Some(s) = args::get(i as usize) {
+            if !first { print!(" "); }
+            print!("{}", s);
+            first = false;
+        }
+    }
+    if newline {
+        println!();
+    }
+}
 sarga_main!(user_main);
